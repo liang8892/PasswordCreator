ea4d61a9348e16631d24ec50540d185a360bac33
liangge <liangge@USER-20161015UB>
2018-03-21 20:55:50 +0800

    V1.0

--- CalcPassword/Form1.Designer.cs
+++ CalcPassword/Form1.Designer.cs
@@ -28,12 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tb_pwd1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_pwd2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_pwdFinal = new System.Windows.Forms.TextBox();
+            this.btn_create = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_pwd1
+            // 
+            this.tb_pwd1.Location = new System.Drawing.Point(108, 32);
+            this.tb_pwd1.Name = "tb_pwd1";
+            this.tb_pwd1.Size = new System.Drawing.Size(344, 21);
+            this.tb_pwd1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "记忆密码";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "区分密码";
+            // 
+            // tb_pwd2
+            // 
+            this.tb_pwd2.Location = new System.Drawing.Point(108, 88);
+            this.tb_pwd2.Name = "tb_pwd2";
+            this.tb_pwd2.Size = new System.Drawing.Size(344, 21);
+            this.tb_pwd2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "生成密码";
+            // 
+            // tb_pwdFinal
+            // 
+            this.tb_pwdFinal.Location = new System.Drawing.Point(108, 145);
+            this.tb_pwdFinal.Name = "tb_pwdFinal";
+            this.tb_pwdFinal.ReadOnly = true;
+            this.tb_pwdFinal.Size = new System.Drawing.Size(344, 21);
+            this.tb_pwdFinal.TabIndex = 4;
+            // 
+            // btn_create
+            // 
+            this.btn_create.Location = new System.Drawing.Point(209, 201);
+            this.btn_create.Name = "btn_create";
+            this.btn_create.Size = new System.Drawing.Size(75, 30);
+            this.btn_create.TabIndex = 6;
+            this.btn_create.Text = "生成";
+            this.btn_create.UseVisualStyleBackColor = true;
+            this.btn_create.Click += new System.EventHandler(this.btn_create_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btn_create;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(498, 255);
+            this.Controls.Add(this.btn_create);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_pwdFinal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_pwd2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_pwd1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "密码生成器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tb_pwd1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_pwd2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_pwdFinal;
+        private System.Windows.Forms.Button btn_create;
     }
 }
 
--- CalcPassword/Form1.cs
+++ CalcPassword/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,25 @@ namespace CalcPassword
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_create_Click(object sender, EventArgs e)
+        {
+            string pwd = "";
+            string pwd1 = tb_pwd1.Text;
+            string pwd2 = tb_pwd2.Text;
+            
+            string feed = pwd1 + "@" + pwd2 + "@8892";
+            byte[] feedToByte = Encoding.UTF8.GetBytes(feed);
+            
+            MD5 md5 = MD5.Create();
+            byte[] hash = md5.ComputeHash(feedToByte);
+            for (int i = 0; i < hash.Length; i++)
+            {
+                //pwd += hash[i].ToString("x2");
+                pwd += hash[i].ToString("X2");
+            }
+            tb_pwdFinal.Text = pwd;
         }
     }
 }
